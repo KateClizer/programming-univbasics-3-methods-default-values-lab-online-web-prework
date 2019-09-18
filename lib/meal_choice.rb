a3a54d26584f89f4815a9299bf31de80c567020e
Katelynne Clizer <kateclizer@gmail.com>
2019-09-18 18:22:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,7 @@
 # Your code here
 def wedding_meal(veg1, veg2, protein = "meat")
 puts "What a nutritious meal!"
-puts 
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+wedding_meal(carrots, brocolli, steak)
